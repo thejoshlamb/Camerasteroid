6e3fc49be2f6f3564e54743f909f575af5900536
thejoshlamb <joshlamb@gmail.com>
2014-01-21 15:03:43 -0500

    chat footer only appears when camera is allowed

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -5,10 +5,7 @@ Template.postSubmit.events({
 		var snap = document.querySelector('#canvas').getContext('2d').drawImage(video, 0, 0, 160, 120);
 		var post = {
 			message: $(e.target).find('[name=message]').val(),
-			photo: function(){
-				if video
-				canvas.toDataURL('image/png')
-			},
+			photo: canvas.toDataURL('image/png'),
 			submitted: new Date().getTime()
 		};
 		
