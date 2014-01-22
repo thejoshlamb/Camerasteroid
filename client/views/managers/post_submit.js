20fb0ebcdb9c77702004ac0d5507425fd1009368
thejoshlamb <joshlamb@gmail.com>
2014-01-22 16:15:04 -0500

    rearranged managers & templates, deleted chaff

---
+++ client/views/managers/post_submit.js
@@ -0,0 +1,73 @@
+//initialize camera on the client side
+if (Meteor.isClient) {
+	// once the postSubmit template is rendered (hidden),
+	// ask the user for access to the webcam
+	Template.postSubmit.rendered = function(){
+
+		var streaming = false,	//	used to watch for camera activation
+		video        = document.querySelector('#video'),
+		canvas       = document.querySelector('#canvas'),
+		width = 160,
+		height = 120;
+
+		navigator.getMedia = ( navigator.getUserMedia ||
+			navigator.webkitGetUserMedia ||
+			navigator.mozGetUserMedia ||
+			navigator.msGetUserMedia);
+
+		// create ObjectURL from stream and play
+		navigator.getMedia(
+			{
+				video: true,
+				audio: false
+			},
+			function(stream) {
+				if (navigator.mozGetUserMedia) {
+					video.mozSrcObject = stream;
+				} else {
+					var vendorURL = window.URL || window.webkitURL;
+					video.src = vendorURL.createObjectURL(stream);
+				}
+				video.play();
+			},
+			function(err) {
+				console.log("An error occured! " + err);
+				document.querySelector(".error").style.visibility = "visible";
+			}
+		);
+
+		//	once camera is active, arrange the chat window
+		video.addEventListener('canplay', function(ev){
+			if (!streaming) {
+				document.querySelector('#footer').style.visibility = "visible";
+				video.setAttribute('width', width);
+				video.setAttribute('height', height);
+				canvas.setAttribute('width', width);
+				canvas.setAttribute('height', height);
+				streaming = true;
+			}
+		}, false);
+	};
+}
+	
+//form submit handler to take pic & save text
+Template.postSubmit.events({
+	'submit form': function(e){
+		e.preventDefault();
+		var video = document.querySelector('#video');
+		var snap = document.querySelector('#canvas').getContext('2d').drawImage(video, 0, 0, 160, 120);
+		var post = {
+			room: location.pathname.substring(1,10),
+			message: $(e.target).find('[name=message]').val(),
+			photo: canvas.toDataURL('image/png'),
+			submitted: new Date()
+		};
+
+		document.querySelector('#chatform').reset();
+		
+		Meteor.call('post', post , function(error,id){
+			if (error)
+				return alert(error.reason);
+		});
+	}
+});
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,8 @@
+// Meteor.isServer can be used to limit where code runs, but it does not prevent 
+// code from being sent to the client. Any sensitive code that you don't want served 
+// to the client, such as code containing passwords or authentication mechanisms,
+// should be kept in the server directory.
+
 Meteor.publish('posts', function(){
 	return Posts.find();
 });
